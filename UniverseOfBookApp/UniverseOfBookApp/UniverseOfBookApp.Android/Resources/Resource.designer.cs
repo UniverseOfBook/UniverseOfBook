1bc6783b918985fd3a56067d59c325eef249a066
rmznsn <ramazansen@eskisehir.edu.tr>
2019-01-13 23:09:19 +0300

    First Page Design

--- UniverseOfBookApp/UniverseOfBookApp/UniverseOfBookApp.Android/Resources/Resource.designer.cs
+++ UniverseOfBookApp/UniverseOfBookApp/UniverseOfBookApp.Android/Resources/Resource.designer.cs
@@ -2363,26 +2363,26 @@ namespace UniverseOfBookApp.Droid
 			// aapt resource value: 0x7f020055
 			public const int avd_hide_password = 2130837589;
 			
-			// aapt resource value: 0x7f020145
-			public const int avd_hide_password_1 = 2130837829;
-			
 			// aapt resource value: 0x7f020146
-			public const int avd_hide_password_2 = 2130837830;
+			public const int avd_hide_password_1 = 2130837830;
 			
 			// aapt resource value: 0x7f020147
-			public const int avd_hide_password_3 = 2130837831;
+			public const int avd_hide_password_2 = 2130837831;
+			
+			// aapt resource value: 0x7f020148
+			public const int avd_hide_password_3 = 2130837832;
 			
 			// aapt resource value: 0x7f020056
 			public const int avd_show_password = 2130837590;
 			
-			// aapt resource value: 0x7f020148
-			public const int avd_show_password_1 = 2130837832;
-			
 			// aapt resource value: 0x7f020149
-			public const int avd_show_password_2 = 2130837833;
+			public const int avd_show_password_1 = 2130837833;
 			
 			// aapt resource value: 0x7f02014a
-			public const int avd_show_password_3 = 2130837834;
+			public const int avd_show_password_2 = 2130837834;
+			
+			// aapt resource value: 0x7f02014b
+			public const int avd_show_password_3 = 2130837835;
 			
 			// aapt resource value: 0x7f020057
 			public const int bookss = 2130837591;
@@ -2955,148 +2955,151 @@ namespace UniverseOfBookApp.Droid
 			public const int @lock = 2130837780;
 			
 			// aapt resource value: 0x7f020115
-			public const int magnifier = 2130837781;
+			public const int Logo = 2130837781;
 			
 			// aapt resource value: 0x7f020116
-			public const int mail = 2130837782;
+			public const int magnifier = 2130837782;
 			
 			// aapt resource value: 0x7f020117
-			public const int man = 2130837783;
+			public const int mail = 2130837783;
 			
 			// aapt resource value: 0x7f020118
-			public const int manuser = 2130837784;
+			public const int man = 2130837784;
 			
 			// aapt resource value: 0x7f020119
-			public const int more = 2130837785;
+			public const int manuser = 2130837785;
 			
 			// aapt resource value: 0x7f02011a
-			public const int mr_button_connected_dark = 2130837786;
+			public const int more = 2130837786;
 			
 			// aapt resource value: 0x7f02011b
-			public const int mr_button_connected_light = 2130837787;
+			public const int mr_button_connected_dark = 2130837787;
 			
 			// aapt resource value: 0x7f02011c
-			public const int mr_button_connecting_dark = 2130837788;
+			public const int mr_button_connected_light = 2130837788;
 			
 			// aapt resource value: 0x7f02011d
-			public const int mr_button_connecting_light = 2130837789;
+			public const int mr_button_connecting_dark = 2130837789;
 			
 			// aapt resource value: 0x7f02011e
-			public const int mr_button_dark = 2130837790;
+			public const int mr_button_connecting_light = 2130837790;
 			
 			// aapt resource value: 0x7f02011f
-			public const int mr_button_light = 2130837791;
+			public const int mr_button_dark = 2130837791;
 			
 			// aapt resource value: 0x7f020120
-			public const int mr_dialog_close_dark = 2130837792;
+			public const int mr_button_light = 2130837792;
 			
 			// aapt resource value: 0x7f020121
-			public const int mr_dialog_close_light = 2130837793;
+			public const int mr_dialog_close_dark = 2130837793;
 			
 			// aapt resource value: 0x7f020122
-			public const int mr_dialog_material_background_dark = 2130837794;
+			public const int mr_dialog_close_light = 2130837794;
 			
 			// aapt resource value: 0x7f020123
-			public const int mr_dialog_material_background_light = 2130837795;
+			public const int mr_dialog_material_background_dark = 2130837795;
 			
 			// aapt resource value: 0x7f020124
-			public const int mr_group_collapse = 2130837796;
+			public const int mr_dialog_material_background_light = 2130837796;
 			
 			// aapt resource value: 0x7f020125
-			public const int mr_group_expand = 2130837797;
+			public const int mr_group_collapse = 2130837797;
 			
 			// aapt resource value: 0x7f020126
-			public const int mr_media_pause_dark = 2130837798;
+			public const int mr_group_expand = 2130837798;
 			
 			// aapt resource value: 0x7f020127
-			public const int mr_media_pause_light = 2130837799;
+			public const int mr_media_pause_dark = 2130837799;
 			
 			// aapt resource value: 0x7f020128
-			public const int mr_media_play_dark = 2130837800;
+			public const int mr_media_pause_light = 2130837800;
 			
 			// aapt resource value: 0x7f020129
-			public const int mr_media_play_light = 2130837801;
+			public const int mr_media_play_dark = 2130837801;
 			
 			// aapt resource value: 0x7f02012a
-			public const int mr_media_stop_dark = 2130837802;
+			public const int mr_media_play_light = 2130837802;
 			
 			// aapt resource value: 0x7f02012b
-			public const int mr_media_stop_light = 2130837803;
+			public const int mr_media_stop_dark = 2130837803;
 			
 			// aapt resource value: 0x7f02012c
-			public const int mr_vol_type_audiotrack_dark = 2130837804;
+			public const int mr_media_stop_light = 2130837804;
 			
 			// aapt resource value: 0x7f02012d
-			public const int mr_vol_type_audiotrack_light = 2130837805;
+			public const int mr_vol_type_audiotrack_dark = 2130837805;
 			
 			// aapt resource value: 0x7f02012e
-			public const int navigation_empty_icon = 2130837806;
+			public const int mr_vol_type_audiotrack_light = 2130837806;
 			
 			// aapt resource value: 0x7f02012f
-			public const int notification_action_background = 2130837807;
+			public const int navigation_empty_icon = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int notification_bg = 2130837808;
+			public const int notification_action_background = 2130837808;
 			
 			// aapt resource value: 0x7f020131
-			public const int notification_bg_low = 2130837809;
+			public const int notification_bg = 2130837809;
 			
 			// aapt resource value: 0x7f020132
-			public const int notification_bg_low_normal = 2130837810;
+			public const int notification_bg_low = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int notification_bg_low_pressed = 2130837811;
+			public const int notification_bg_low_normal = 2130837811;
 			
 			// aapt resource value: 0x7f020134
-			public const int notification_bg_normal = 2130837812;
+			public const int notification_bg_low_pressed = 2130837812;
 			
 			// aapt resource value: 0x7f020135
-			public const int notification_bg_normal_pressed = 2130837813;
+			public const int notification_bg_normal = 2130837813;
 			
 			// aapt resource value: 0x7f020136
-			public const int notification_icon_background = 2130837814;
-			
-			// aapt resource value: 0x7f020143
-			public const int notification_template_icon_bg = 2130837827;
-			
-			// aapt resource value: 0x7f020144
-			public const int notification_template_icon_low_bg = 2130837828;
+			public const int notification_bg_normal_pressed = 2130837814;
 			
 			// aapt resource value: 0x7f020137
-			public const int notification_tile_bg = 2130837815;
+			public const int notification_icon_background = 2130837815;
+			
+			// aapt resource value: 0x7f020144
+			public const int notification_template_icon_bg = 2130837828;
+			
+			// aapt resource value: 0x7f020145
+			public const int notification_template_icon_low_bg = 2130837829;
 			
 			// aapt resource value: 0x7f020138
-			public const int notify_panel_notification_icon_bg = 2130837816;
+			public const int notification_tile_bg = 2130837816;
 			
 			// aapt resource value: 0x7f020139
-			public const int photo = 2130837817;
+			public const int notify_panel_notification_icon_bg = 2130837817;
 			
 			// aapt resource value: 0x7f02013a
-			public const int reading = 2130837818;
+			public const int photo = 2130837818;
 			
 			// aapt resource value: 0x7f02013b
-			public const int search = 2130837819;
+			public const int reading = 2130837819;
 			
 			// aapt resource value: 0x7f02013c
-			public const int settings = 2130837820;
+			public const int search = 2130837820;
 			
 			// aapt resource value: 0x7f02013d
-			public const int SucveCeza = 2130837821;
+			public const int settings = 2130837821;
 			
 			// aapt resource value: 0x7f02013e
-			public const int tooltip_frame_dark = 2130837822;
+			public const int SucveCeza = 2130837822;
 			
 			// aapt resource value: 0x7f02013f
-			public const int tooltip_frame_light = 2130837823;
+			public const int tooltip_frame_dark = 2130837823;
 			
 			// aapt resource value: 0x7f020140
-			public const int user = 2130837824;
+			public const int tooltip_frame_light = 2130837824;
 			
 			// aapt resource value: 0x7f020141
-			public const int writer = 2130837825;
+			public const int user = 2130837825;
 			
 			// aapt resource value: 0x7f020142
-			public const int writing = 2130837826;
+			public const int writer = 2130837826;
+			
+			// aapt resource value: 0x7f020143
+			public const int writing = 2130837827;
 			
 			static Drawable()
 			{
@@ -4254,8 +4257,8 @@ namespace UniverseOfBookApp.Droid
 			// aapt resource value: 0x7f0d016e
 			public const int Animation_Design_BottomSheetDialog = 2131558766;
 			
-			// aapt resource value: 0x7f0d0191
-			public const int AppCompatDialogStyle = 2131558801;
+			// aapt resource value: 0x7f0d0192
+			public const int AppCompatDialogStyle = 2131558802;
 			
 			// aapt resource value: 0x7f0d00a9
 			public const int Base_AlertDialog_AppCompat = 2131558569;
@@ -4758,14 +4761,14 @@ namespace UniverseOfBookApp.Droid
 			// aapt resource value: 0x7f0d000e
 			public const int CardView_Light = 2131558414;
 			
-			// aapt resource value: 0x7f0d0192
-			public const int DrawerArrowStyle = 2131558802;
-			
-			// aapt resource value: 0x7f0d018f
-			public const int MainTheme = 2131558799;
+			// aapt resource value: 0x7f0d0193
+			public const int DrawerArrowStyle = 2131558803;
 			
 			// aapt resource value: 0x7f0d0190
-			public const int MainTheme_Base = 2131558800;
+			public const int MainTheme = 2131558800;
+			
+			// aapt resource value: 0x7f0d0191
+			public const int MainTheme_Base = 2131558801;
 			
 			// aapt resource value: 0x7f0d002d
 			public const int Platform_AppCompat = 2131558445;
@@ -5444,6 +5447,9 @@ namespace UniverseOfBookApp.Droid
 			
 			// aapt resource value: 0x7f0d000a
 			public const int Widget_MediaRouter_MediaRouteButton = 2131558410;
+			
+			// aapt resource value: 0x7f0d018f
+			public const int splashscreen = 2131558799;
 			
 			static Style()
 			{
